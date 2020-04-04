28cf2d79630b3b52c81a11f47106dd3f370141bd
candycrushpro <bradleynilmar6702@gmail.com>
2020-04-04 10:27:08 +0300

    changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quote: Quote[] = [
-  new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    new Quote(1,'Vincent Havard','No pain no gain',new Date(2020,3,14)),
+    new Quote(2,'Jacob Wasonga','Be the type of person that you would like to meet.',new Date(2019,6,9)),
+    new Quote(3,'Steve Havey','The sadest thing is when you are feeling real down,you realise that there is no shoulder for you to lean on',new Date(2022,1,12)),
+    // new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    // new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    // new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
   quotes: any;
   toggleDetails(index: string | number){
